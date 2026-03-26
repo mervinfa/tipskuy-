addfa33b80195354eab2cc79c01597b8bb9c2208
mervin fauzhan atkly <merpin4060@gmail.com>
2026-03-26 11:54:53 +0700

    feat : initial page driver

---
+++ app/driver/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DriverPage = () => {
+  return (
+    <div>
+      Halaman Driver
+    </div>
+  );
+};
+
+// INI YANG PALING PENTING DAN BIKIN ERROR TADI:
+export default DriverPage;
